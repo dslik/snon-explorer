574446446760c8b8096936c898c499f9b5ee4e81
David Slik <dslik@acm.org>
2023-09-27 22:19:34 -0700

    Initial checkin of SNON visualizer

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # snon-explorer
+
 Reads SNON fragments from devices and visualizes them as a graph
+
+## Running the visualizer
+
+Step 1:
+
+Generate the SNON data dump by running the following Ruby program:
+
+```
+% ruby snon-dump.rb
+```
+
+Step 2:
+
+Run a local web browser using Python:
+
+```
+python3 -m http.server
+```
+
+Step 3:
+
+Open http://localhost:8000/ in your web browser
+
+
+## Syntax of JSON file
+
+Based on Vis documentation: https://github.com/almende/vis/blob/master/examples/network/data/datasets.html
+
+```
+{
+	"nodes": [
+ 		{"id": 1, "label": "Node 1", "title": "Title popup<br>on mouseover"},
+        {"id": 2, "label": "Node 2"},
+        {"id": 3, "label": "Node 3"},
+        {"id": 4, "label": "Node 4", "color": "#228888"},
+        {"id": 5, "label": "Node 5"}
+	],
+	"edges": [
+		{"from": 1, "to": 3, "label": "label"},
+        {"from": 1, "to": 2},
+        {"from": 2, "to": 4, "arrows": "to"},
+        {"from": 2, "to": 5}
+	]
+}
+```
---
+++ snon-dump.rb
@@ -0,0 +1,53 @@
+# Dumps SNON data to a JSON graph representation
+require "json"
+require "socket"
+
+# Get SNON data
+print "Connecting to SNON device\n";
+socket = TCPSocket.open("192.168.88.101", "200");
+socket.print("{}\r\n");
+
+print "Reading SNON date\n";
+response = socket.gets;
+response = socket.gets;
+socket.close;
+
+# Populate graph
+print "Generating graph\n";
+snon_data = JSON.parse(response);
+snon_entities = Array.new();
+snon_relations = Array.new();
+
+snon_data.each do |fragment|
+	node = Hash.new();
+	node["id"] = fragment["eID"];
+	node["label"] = fragment["eN"]["*"];
+	node["group"] = fragment["eC"];
+	node["title"] = "<pre>" + JSON.pretty_generate(fragment).gsub("\n", "<br>") + "</pre>";
+
+	snon_entities << node;
+end
+
+snon_data.each do |fragment|
+	if(fragment["eR"])
+		fragment["eR"].each do |relation|
+			rel = Hash.new();
+			rel["from"] = fragment["eID"];
+			rel["to"] = relation[1][0];
+			rel["arrows"] = "to";
+			rel["label"] = relation[0];
+
+			snon_relations << rel;
+		end
+	end
+end
+
+graph = Hash.new();
+graph["nodes"] = snon_entities;
+graph["edges"] = snon_relations;
+
+# Write out the JSON
+print "Writing graph to graph.json\n";
+file = File.open("graph.json", "w");
+file.write(JSON.pretty_generate(graph));
+file.close();
